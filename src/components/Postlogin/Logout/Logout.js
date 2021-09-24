f7b96aa92d1853f1a0052c7194cdac2c380ce2a0
Sruthi Raj <sruthiraj97sruthizzzz@gmail.com>
2021-09-24 22:12:12 +0530

    commit

--- src/components/Postlogin/Logout/Logout.js
+++ src/components/Postlogin/Logout/Logout.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 function Logout(props) {
 
-    function popupOk(){
+    function popupOk() {
         localStorage.removeItem("Authentication");
         localStorage.removeItem('Username')
-        localStorage.setItem("Authentication",false)
+        localStorage.setItem("Authentication", false)
         props.history.push("/Login")
     }
     return (
@@ -13,8 +13,8 @@ function Logout(props) {
             <div className="logoutbox">
                 <span className="logoutclose-icon" onClick={props.handleClose}>x</span>
                 <b>Are you sure you want to exit?</b> <br></br>
-                        <br></br><button id='button' onClick={popupOk} >Ok</button> &nbsp;
-                        <button id='button' onClick={props.handleClose}>Cancel</button>
+                <br></br><button id='button' onClick={popupOk} >Ok</button> &nbsp;
+                <button id='button' onClick={props.handleClose}>Cancel</button>
             </div>
 
         </div>
